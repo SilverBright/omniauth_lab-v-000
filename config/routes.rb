f891e8e0d44ee3fcbb6e3cad0a367ea08084a010
Barbara Washington <barbara.m.washington@gmail.com>
2018-09-21 18:26:40 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  resources :sessions
+  resources :users
 
   
   # The priority is based upon order of creation: first created -> highest priority.
