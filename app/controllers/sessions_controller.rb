7a4348d4fd15c25b2bdb5e0b28197577118c0467
Barbara Washington <barbara.m.washington@gmail.com>
2018-09-21 18:32:34 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,9 @@ class SessionsController < ApplicationController
 
    def create
     auth_hash = request.env["omniauth.auth"]["info"]
-    # {"email"=>"blake@flatironschool.com", "name"=>"Blake Johnson"}
     user = User.find_or_create_by(name: auth_hash["name"])
     session[:user_id] = user.id
 
     redirect_to root_path
-
-
-
   end
 end
